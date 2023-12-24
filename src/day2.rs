c178fedbe1e62d61c2bd595cd5f7cf6076b6a0fd
dasdias <das.dias6@gmail.com>
2023-12-24 02:39:26 +0000

    Stuck in Day3, Part 1

---
+++ src/day2.rs
@@ -0,0 +1,127 @@
+use std::fs::read_to_string;
+use std::collections::HashMap;
+struct Game {
+  id: usize,
+  draws: Vec<HashMap<String, usize>>
+}
+pub struct Config {
+  pub max_red: usize,
+  pub max_green: usize,
+  pub max_blue: usize,
+}
+fn parse_game(line: &str) -> Game {
+  let mut draws = Vec::new();
+  let gameid_draw: Vec<& str> = line.split(":").collect();
+  let id: usize = gameid_draw[0]
+    .split(" ")
+    .collect::<Vec<& str>>()[1].parse().unwrap();
+  let draw_set = gameid_draw[1].split(";");
+  for draw_subset in draw_set {
+    let cube_draws = draw_subset.split(",");
+    let mut draw = HashMap::new();
+    cube_draws.into_iter().for_each(|cd| {
+      let cube_draw: Vec<& str> = cd.split(" ").collect();
+      let cube_color = String::from(cube_draw[2]);
+      let cube_draw_number: usize = cube_draw[1].parse().unwrap();
+      draw.insert(cube_color, cube_draw_number);
+    });
+    draws.push(draw);
+  }
+  Game {
+    id,
+    draws
+  }
+}
+fn is_valid_game(game: &Game, config: &Config) -> bool {
+  for draw in &game.draws {
+    let draw_inspections: Vec<bool> = draw.iter().map( 
+      |(color, draw_number)| -> bool {
+        match color.as_str() {
+          "red" => draw_number > &config.max_red,
+          "green" => draw_number > &config.max_green,
+          "blue" => draw_number > &config.max_blue,
+          _ => false
+        }
+    }).collect();
+    if draw_inspections.iter().any(|val| *val) {return false;}
+  }
+  true
+}
+pub fn get_possible_games_id_sum(filename: & str, config: Config) -> usize {
+  let mut result = 0;
+  let mut possible_games: Vec<Game> = Vec::new();
+  read_to_string(filename) 
+    .unwrap()
+    .lines()
+    .for_each(|line| {
+      let game = parse_game(line);
+      if is_valid_game(&game, &config){ possible_games.push(game); }
+    });
+  possible_games.iter().for_each(|g| result += g.id);
+  result
+}
+fn get_fewest_cubes_power(game: &Game) -> usize {
+  let mut fewest_red: usize = 0;
+  let mut fewest_green: usize = 0;
+  let mut fewest_blue: usize = 0;
+  for draw in &game.draws {
+    draw.iter().for_each(|(color, num)| {
+      match color.as_str() {
+        "red"   => if *num > fewest_red {fewest_red = *num} ,
+        "green" => if *num > fewest_green {fewest_green = *num} ,
+        "blue"  => if *num > fewest_blue {fewest_blue = *num} ,
+        _ => ()
+      }
+    });
+  }
+  fewest_red * fewest_green * fewest_blue
+}
+pub fn get_fewest_cubes_power_sum(filename: & str) -> usize {
+  let mut result = 0;
+  read_to_string(filename) 
+    .unwrap()
+    .lines()
+    .for_each(|line| {
+      let game = parse_game(line);
+      result += get_fewest_cubes_power(&game);
+    });
+  result
+}
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  fn test_parse_game() {
+    let game = parse_game("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
+    let game_draw1 = HashMap::from([
+      (String::from("blue"), 3),
+      (String::from("red"), 4)
+    ]);
+    assert_eq!(game.id, 1);
+    assert_eq!(
+      game.draws[0], 
+      game_draw1
+    );
+  }
+  #[test]
+  fn test_is_valid_game() {
+    let game = parse_game("Game 1: 3 blue, 15 red; 1 red, 2 green, 6 blue; 2 green");
+    let config1 = Config {
+      max_blue: 10,
+      max_green: 10,
+      max_red: 10
+    };
+    let config2 = Config {
+      max_blue: 10,
+      max_green: 10,
+      max_red: 20
+    };
+    assert_eq!(is_valid_game(&game, &config1), false);
+    assert_eq!(is_valid_game(&game, &config2), true);
+  }
+  #[test]
+  fn test_get_fewest_cubes_power() {
+    let game = parse_game("Game 1: 3 blue, 15 red; 1 red, 2 green, 6 blue; 2 green");
+    assert_eq!(get_fewest_cubes_power(&game), 15 * 2 * 6);
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod day1;
-use day1::get_calibration_digits;
-use day2::{get_possible_games_id_sum, Config};
+mod day2;
 
-use crate::day2::get_fewest_cubes_power_sum;
+use day1::get_calibration_digits;
+use day2::{get_possible_games_id_sum, get_fewest_cubes_power_sum, Config};
+use day3::{get_part_number_sum};
 
 fn main() {
   println!("Advent of Code 2023!");
@@ -18,103 +19,110 @@ fn main() {
   println!(
     "Day 2 Part 2 Answer: {}", 
     get_fewest_cubes_power_sum("resources/adventofcode.com_2023_day_2_input.txt")
+  );
+  println!(
+    "Day 3 Part 1 Answer: {}", 
+    get_part_number_sum("resources/adventofcode.com_2023_day_3_input.txt")
   )
 }
 
-mod day2 {
+mod day3 {
   use std::fs::read_to_string;
   use std::collections::HashMap;
-  struct Game {
-    id: usize,
-    draws: Vec<HashMap<String, usize>>
+
+  const SYMBOLS: [char; 14] = ['!','\"','#','$','%','&','/','(',')','=','?','*','+', '@'];
+  type Symbol = usize; /* column */
+  type PartNumber = (usize, i64); /* number of digits, number */
+
+  fn parse_symbols(line: & str) -> Vec<Symbol> {
+    line.match_indices(&SYMBOLS)
+      .into_iter()
+      .map(|(col, _)| col)
+      .collect()
   }
 
-  pub struct Config {
-    pub max_red: usize,
-    pub max_green: usize,
-    pub max_blue: usize,
-  }
-
-  fn parse_game(line: &str) -> Game {
-    let mut draws = Vec::new();
-    let gameid_draw: Vec<& str> = line.split(":").collect();
-    let id: usize = gameid_draw[0]
-      .split(" ")
-      .collect::<Vec<& str>>()[1].parse().unwrap();
-    let draw_set = gameid_draw[1].split(";");
-    for draw_subset in draw_set {
-      let cube_draws = draw_subset.split(",");
-      let mut draw = HashMap::new();
-      cube_draws.into_iter().for_each(|cd| {
-        let cube_draw: Vec<& str> = cd.split(" ").collect();
-        let cube_color = String::from(cube_draw[2]);
-        let cube_draw_number: usize = cube_draw[1].parse().unwrap();
-        draw.insert(cube_color, cube_draw_number);
-      });
-      draws.push(draw);
+  fn parse_numbers(line: &str) -> HashMap<usize, PartNumber> { 
+    let mut result: HashMap<usize, PartNumber> = HashMap::new();
+    let numbers = String::from(line).replace(&SYMBOLS, ".");
+    let numbers = numbers.split(".");
+    for number_str in numbers {
+      if (number_str.len() > 0) && (number_str != "-") {
+        let mut digits: Vec<char> = number_str.chars().collect();
+        if digits[digits.len()-1] == '-' {
+          digits.remove(digits.len()-1);
+        }
+       let number = String::from_iter(digits);
+        result.insert(
+          line.find(&number).unwrap(),
+          (number.len(), number.parse().unwrap())
+        );
+      }
     }
-    Game {
-      id,
-      draws
-    }
-  }
-
-  fn is_valid_game(game: &Game, config: &Config) -> bool {
-    for draw in &game.draws {
-      let draw_inspections: Vec<bool> = draw.iter().map( 
-        |(color, draw_number)| -> bool {
-          match color.as_str() {
-            "red" => draw_number > &config.max_red,
-            "green" => draw_number > &config.max_green,
-            "blue" => draw_number > &config.max_blue,
-            _ => false
-          }
-      }).collect();
-      if draw_inspections.iter().any(|val| *val) {return false;}
-    }
-    true
-  }
-
-  pub fn get_possible_games_id_sum(filename: & str, config: Config) -> usize {
-    let mut result = 0;
-    let mut possible_games: Vec<Game> = Vec::new();
-    read_to_string(filename) 
-      .unwrap()
-      .lines()
-      .for_each(|line| {
-        let game = parse_game(line);
-        if is_valid_game(&game, &config){ possible_games.push(game); }
-      });
-    possible_games.iter().for_each(|g| result += g.id);
     result
   }
 
-  fn get_fewest_cubes_power(game: &Game) -> usize {
-    let mut fewest_red: usize = 0;
-    let mut fewest_green: usize = 0;
-    let mut fewest_blue: usize = 0;
-    for draw in &game.draws {
-      draw.iter().for_each(|(color, num)| {
-        match color.as_str() {
-          "red"   => if *num > fewest_red {fewest_red = *num} ,
-          "green" => if *num > fewest_green {fewest_green = *num} ,
-          "blue"  => if *num > fewest_blue {fewest_blue = *num} ,
-          _ => ()
-        }
-      });
-    }
-    fewest_red * fewest_green * fewest_blue
+  pub fn get_part_number_sum(filename: &str) -> i64 {
+    let content = read_to_string(filename)
+      .unwrap();
+    let lines = content.lines()
+      .collect();
+    find_part_number_sum(lines)
   }
 
-  pub fn get_fewest_cubes_power_sum(filename: & str) -> usize {
+  fn find_part_number_sum(lines: Vec<& str>) -> i64 { 
     let mut result = 0;
-    read_to_string(filename) 
-      .unwrap()
-      .lines()
-      .for_each(|line| {
-        let game = parse_game(line);
-        result += get_fewest_cubes_power(&game);
+    let mut numbers: HashMap<usize, HashMap<usize, PartNumber>> = HashMap::new();
+    let mut symbols: Vec<(usize, Symbol)> = Vec::new();
+    lines
+      .iter()
+      .enumerate()
+      .for_each(|(lid, line)| {
+        numbers.insert(lid, parse_numbers(line));
+        for symbol in parse_symbols(line) {
+          symbols.push((lid, symbol));
+        }
       });
+    let mut possible_symbol_positions: Vec<(usize, usize)>;
+    let mut valid_part_numbers: Vec<(usize,usize, i64)> = Vec::new(); /* line, col, number */
+    for (lid, part_numbers) in numbers {
+      for (col, part_number) in part_numbers {
+        /* slow algorithm: check first column and first row inside nested loop */
+        for digit_idx in 0..(part_number.0) {
+          let cid = col + digit_idx;
+          if (lid > 0) && (cid > 0) {
+            possible_symbol_positions = vec![
+              (lid-1, cid-1), (lid-1, cid), (lid-1, cid+1),
+              (lid, cid-1),   /* number */  (lid, cid+1),
+              (lid+1, cid-1), (lid+1, cid), (lid+1, cid+1),
+            ];
+          } else if lid > 0 {
+            possible_symbol_positions = vec![
+              (lid-1, cid), (lid-1, cid+1),
+              /* number */  (lid, cid+1),
+              (lid+1, cid), (lid+1, cid+1),
+            ];
+          } else if cid > 0 {
+            possible_symbol_positions = vec![
+              (lid, cid-1),   /* number */  (lid, cid+1),
+              (lid+1, cid-1), (lid+1, cid), (lid+1, cid+1),
+            ];
+          } else {
+            possible_symbol_positions = vec![
+              /* number */  (lid, cid+1),
+              (lid+1, cid), (lid+1, cid+1),
+            ];
+          }
+          if possible_symbol_positions.iter().any(|pos| symbols.contains(&pos)) {
+            if !valid_part_numbers.contains(&(lid, col, part_number.1)) {
+              valid_part_numbers.push((lid, col, part_number.1))
+            }
+          }
+        }
+      }
+    }
+    valid_part_numbers
+      .iter()
+      .for_each(|(_, _, num)| result += num);
     result
   }
 
@@ -122,40 +130,40 @@ mod day2 {
   mod tests {
     use super::*;
     #[test]
-    fn test_parse_game() {
-      let game = parse_game("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
-      let game_draw1 = HashMap::from([
-        (String::from("blue"), 3),
-        (String::from("red"), 4)
+    fn test_parse_symbols() {
+      let symbols = parse_symbols("467..114..");
+      assert_eq!( symbols, vec![] );
+      let symbols = parse_symbols("467*.114..");
+      assert_eq!( symbols, vec![3] );
+      let symbols = parse_symbols("...$.*....");
+      assert_eq!( symbols, vec![3, 5] );
+    }
+
+    #[test]
+    fn test_parse_numbers() {
+      let numbers = parse_numbers("467..114..");
+      assert_eq!( numbers, HashMap::from([(0, (3, 467)), (5, (3, 114))]) );
+      let numbers = parse_numbers("467*.114..");
+      assert_eq!( numbers, HashMap::from([(0, (3, 467)), (5, (3, 114))]) );
+      let numbers = parse_numbers("...$.*....");
+      assert_eq!( numbers, HashMap::new() );
+    }
+
+    #[test]
+    fn test_find_part_number_sum() {
+      let sum = find_part_number_sum(vec![
+        "467..114..",
+        "...*......",
+        "..35..633.",
+        "......#...",
+        "617*......",
+        ".....+.58.",
+        "..592.....",
+        "......755.",
+        "...$.*....",
+        ".664.598.."
       ]);
-      assert_eq!(game.id, 1);
-      assert_eq!(
-        game.draws[0], 
-        game_draw1
-      );
-    }
-
-    #[test]
-    fn test_is_valid_game() {
-      let game = parse_game("Game 1: 3 blue, 15 red; 1 red, 2 green, 6 blue; 2 green");
-      let config1 = Config {
-        max_blue: 10,
-        max_green: 10,
-        max_red: 10
-      };
-      let config2 = Config {
-        max_blue: 10,
-        max_green: 10,
-        max_red: 20
-      };
-      assert_eq!(is_valid_game(&game, &config1), false);
-      assert_eq!(is_valid_game(&game, &config2), true);
-    }
-
-    #[test]
-    fn test_get_fewest_cubes_power() {
-      let game = parse_game("Game 1: 3 blue, 15 red; 1 red, 2 green, 6 blue; 2 green");
-      assert_eq!(get_fewest_cubes_power(&game), 15 * 2 * 6);
+      assert_eq!(sum, 4361)
     }
   }
 }
